1f56773490585c4a27768a78783bc959e7d236a3
Amauryiptv <86899416+Amauryiptv@users.noreply.github.com>
2024-01-10 01:55:26 -0300

    Create Apocalipsis.py

---
+++ Apocalipsis.py
@@ -0,0 +1,130 @@
+importar sistema operacional, pip
+importar data e hora, os
+importar soquete, hashlib, shutil
+importar json, aleatório, sys, tempo, re, marechal
+apelido=""
+apelido=""
+se apelido=="":
+nickn="▞▞▞▞▞▖🅸🅿🆃🆅🅼🅰🆂🆃🅴🆁▝▞▞▞▞▞"
+tentar:
+importar androidhelper como sl4a
+anúncio = sl4a.Android()
+exceto:passar
+
+subprocesso de importação
+tentar:
+importar threading
+exceto:passar
+importar pathlib,base64
+#subprocess.run(["limpar", ""])
+tentar:
+solicitações de importação
+exceto:
+print("O módulo não foi instalado \n O módulo foi instalado... \n")
+pip.main(['instalar', 'solicitações'])
+solicitações de importação
+tentar:
+        
+importar meia
+exceto:
+print("O módulo sock não foi instalado \n O módulo sock foi instalado \n")
+pip.main(['instalar', 'solicitações[meias]'] )
+pip.main(['instalar', 'meia'] )
+pip.main(['instalar', 'meias'] )
+pip.main(['instalar', 'PySocks'] )
+importar meia
+tentar:
+de playsound import playsound#import solicitações
+exceto:
+print("O módulo não foi instalado \n O módulo foi instalado... \n")
+pip.main(['instalar', 'playsound'])
+
+#subprocess.run(["limpar", ""])
+getmac=""
+oto=0
+tur=0
+Série = ""
+csay = 0
+registro de importação
+de requests.packages.urllib3.exceptions importar InsecureRequestWarning
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS="TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384:TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:TLS_ECDHE_ RSA_WITH_AES_128_GCM_SHA256:TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256:TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256:TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:TLS_ECDHE _RSA_WITH_AES_256_GCM_SHA384:TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA:TLS_ECDHE_RSA_WITH_AES_2 56_CBC_SHA:TLS_RSA_WITH_AES_128_GCM_SHA256:TLS_RSA_WITH_AES_256_GCM_SHA384:TLS_RSA_WITH_AES_128_CBC_SHA:TLS_RSA_WITH_AES_256_CBC_SHA:TLS_RSA_WITH_3DES_EDE_CBC _SHA:TLS13 -CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:ECDHE:!COMP:TLS13-AES-256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256: TLS13-AES-128-GCM-SHA256"
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+de uuid importar getnode como get_mac
+#mac = "1102274947"#str(get_mac())
+tentar:
+importar cfscrape
+sesq= solicitações.Sessão()
+ses = cfscrape.create_scraper(sess=sesq)
+exceto:
+ses= solicitações.Sessão()
+logging.captureWarnings(Verdadeiro)
+os.system('cls')
+
+diga1 = 0
+diga2 = 0
+dizer=0
+yanpanel="hata"
+imzayan = ""
+bul=0
+hit = 0
+próx = 0
+cpm=0
+
+#from datetime importar datahora
+#bugun=(datetime.today().strftime('%d-%m-%Y'))
+
+# #f bugun>= '09-04-2022':
+#		imprimir("""
+# Otkriveno je vremensko ogranicenje, kotaktirajte proizvođaca
+#@IptvMaster""")
+#		desistir()
+
+
+# macSayisi=999999999999991#1#deneme sayisı
+feyzo=("""
+\33[1;44m
+                   APOCALIPSE
+            MAC SCANER VELIKE BRZINE
+                                                
+                                                 \33[0m\33[31m\33[1;37;41m
+\33[1;7;42m MOD BALKAN POR @IPTVMASTER \33[0m """)
+imprimir (feyzo)
+#id1='aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlJOUFNBQ3RxOUNzZ0hzRzlaYlFp'
+#id2='VG1WM1N6QmtVVWhuWkRGa1EyVndNV2wzZGtwMVNYRlJORWh4WW5kaFozSjFiVGx4UWkxUU5EQndVbGRFYTNRMFdWVlNjakZZTUdWaFRTOXdkV0pvZEcxcw=='
+#id3=base64.b64decode(id1)+base64.b64decode(base64.b64decode(id2))
+#res = ses.get(id3, tempo limite=15, verificar=Falso)
+
+#si=(str(res.text).split('sifre')[1].split('paranormal')[0])
+#si=si.replace('\n','')
+#exec(base64.b64decode(base64.b64decode('ZFhsbGJHbHJQU0lpQ21admNpQnBJR2x1SUhOMGNpaHlaWE11ZEdWNGRDa3VjM0JzYVhRb0p6eDBaQ0JqYkdGemN6MGljekVpSUdScGNqMGliSFJ5SWo0bktU b0tDV2xtSUNjOEwzUmtQangwWkNCamJHRnpjejBpY3pBaUlHUnBjajBpYkhSeUlqNG5JR2x1SUdrNkNpTm1iM0lnYVNCcGJpQnphUzV6Y0d4cGRDZ25QR0p5SUM4K0p5azZDZ2tKWjJWMGJXRmp QU2hwTG5Od2JHbDBLQ2M4TDNSa1BqeDBaQ0JqYkdGemN6MGljekFpSUdScGNqMGliSFJ5SWo0bktWc3dYU2tLQ1FramNISnBiblFvWjJWMGJXRmpLUW9KQ1dsbUlHMWhZeUE5UFNCemRISW9aMlYwYldGakt Ub0tDUWtKZFhsbGJHbHJQU0oyYVhBaUNna0pDU051YVdOcmJqMG9hUzV6Y0d4cGRDZ25QQzkwWkQ0OGRHUWdZMnhoYzNNOUluTXdJaUJrYVhJOUlteDBjaUkrSnlsYk1WMHVjM0JzYVhRb0p6d3ZkR 1ErUEM5MGNqNG5LVnN3WFNrS0NRa0pZbkpsWVdzS2FXWWdkWGxsYkdsclBUMGlJam9LQ1hCeWFXNTBLQ0pCeFo5aHhKL0VzV1JoSUhsaGVtRnVJRWxrSUU1dklIbDFJRmd0UkdWbGNDQmhaRzFw Ym1VZ2FXeGxkR2x1TGk0dUlpa0tDWEJ5YVc1MEtDSkpaQ0JPYnpvZ0lpdHpkSElvYldGaktTa0tDWEYxYVhRb0tRcHJhWE5oWTJscmRHazlJaUlLY0dGMGRHVnliajBnSWloY2Qzc3lmVHBjZDNze WZUcGNkM3N5ZlRwY2Qzc3lmVHBjZDNzeWZUcGNkM3N5ZlNraUNtOTZaV3h0WVdNOULpSUs=')))
+
+padrão= "(\w{2}:\w{2}:\w{2}:\w{2}:\w{2}:\w{2})"
+
+
+#################
+
+nick='@FeyzullahK'
+bekleme = 1
+isimle=""
+#subprocess.run(["limpar", ""])
+ 
+#print tptv.cz:80
+introdução = """
+    1 ➨ portal.php
+    2 ➨ servidor/load.php
+    3 ➨ stalker_portal.php
+    4 ➨ maglove.php
+    5 ➨ magaccess.php
+    6 ➨ portalcc.php
+    7 ➨ ministra.php
+    8 ➨ xUi /c/server/load.php
+    9 ➨ xUi /c/portal.php
+  10 ➨ portalstb/portal.php
+  11 ➨ mag.portal.php
+  12 ➨ Link_OK.php
+  13 ➨k/portal.php
+
+\33[1;44m
+Odaberite 1 do 13 para o primeiro portal = \33[0m\33[31m\33[1;37;41m"""
+
+a="""Portal:P
