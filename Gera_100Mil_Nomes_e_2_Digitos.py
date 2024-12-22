8aa34384917a8f110a74c715833e400725f5a717
Amauryiptv <86899416+Amauryiptv@users.noreply.github.com>
2024-12-22 10:45:12 -0300

    Update Gera_100Mil_Nomes_e_hdh_Digitos.py

--- Gera_100Mil_Nomes_e_2_Digitos.py
+++ Gera_100Mil_Nomes_e_2_Digitos.py
@@ -10,8 +10,7 @@ def gerar_usuarios_e_senhas(qtd):
         nome = fake.first_name().lower()  # Nome em minúsculas
         digito = str(random.randint(0,9)).zfill(1)  # Gera um número de dois dígitos
         usuario = f"{nome}"
-        senha = usuario  # Senha igual ao usuário
-        usuarios_senhas.append(f"{usuario}:{senha}")
+        usuarios_senhas.append(f"{usuario}")
 
     return usuarios_senhas
 
@@ -20,7 +19,7 @@ usuarios_senhas = gerar_usuarios_e_senhas(100000)
 print("Gerando Arquivo.")
 
 # Salva em um arquivo
-with open("/sdcard/combo/usuarios_senhas1.txt", "w") as f:
+with open("/sdcard/combo/usuarios_senhas2.txt", "w") as f:
     for usuario_senha in usuarios_senhas:
         f.write(usuario_senha + "\n")
 
